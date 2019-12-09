107fd6a033d57adcda8b80993cba95011a6edc09
AtulKaushik-hub <atulkau99@gmail.com>
2019-12-09 12:01:12 +0530

    Updates

---
+++ precache-manifest.46451097cb072c60de60a634ee7e31cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400e8375cb1921dda0049fcd982184ab",
+    "url": "/test-react-deploy5/index.html"
+  },
+  {
+    "revision": "5bd1e348d8037d535b02",
+    "url": "/test-react-deploy5/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2d8e10d132ab4b3c99e2",
+    "url": "/test-react-deploy5/static/js/2.ba3d716e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-react-deploy5/static/js/2.ba3d716e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bd1e348d8037d535b02",
+    "url": "/test-react-deploy5/static/js/main.07dca691.chunk.js"
+  },
+  {
+    "revision": "90bdfc7035a184f65512",
+    "url": "/test-react-deploy5/static/js/runtime-main.d016ce99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-deploy5/static/media/logo.5d5d9eef.svg"
+  }
+]);
